fb95487ac94bdad4ab8d8b39b6efed7c7c12285b
farsheel <farsheelpkl@gmail.com>
2017-08-07 14:44:04 +0530

    bug fixes

---
+++ app/src/main/java/com/precoders/adminpanelfinder/SplashScreen.java
@@ -0,0 +1,26 @@
+package com.precoders.adminpanelfinder;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    public long TIME=3000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent in=new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(in);
+                finish();
+            }
+        },TIME);
+
+    }
+}
